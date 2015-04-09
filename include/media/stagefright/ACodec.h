5c4ef5304b0aa407b120629980fedf3b55dab351
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2015-04-09 11:13:47 +0530

    libstagefright: Fix for testException
    Handled decoder configure call for encoder
    component and vice-versa in fallback logic
    
    CRs-Fixed: 891538
    Change-Id: Ibb0d2da829a0e0f907ad8265836bac0466de1b4d

--- include/media/stagefright/ACodec.h
+++ include/media/stagefright/ACodec.h
@@ -262,6 +262,8 @@ protected:
     bool mSentFormat;
     bool mIsVideo;
     bool mIsEncoder;
+    bool mEncoderComponent;
+    bool mComponentAllocByName;
     bool mFatalError;
     bool mShutdownInProgress;
     bool mExplicitShutdown;
