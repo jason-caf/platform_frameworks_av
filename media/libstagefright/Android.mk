b99c18a8860e50a0e338f7382492ee4b5364e87d
Andreas Schneider <asn@cryptomilk.org>
2015-08-17 09:58:47 +0200

    libstagefright: Add support for NV21 color format
    This adds the native color format 'OMX_SEC_COLOR_FormatNV21Linear' of
    exynos5 which is NV21 and needed for the camera. You can turn it on
    with:
    
    BOARD_USE_SAMSUNG_COLORFORMAT_NV21 := true
    
    It requires
    
    const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "nv21";
    
    to be specified in CameraExtraParameters.h.
    
    Change-Id: I64ca86d074468b1e7a2958a73c3c0380e64a9fc0
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -211,6 +211,14 @@ ifeq ($(DTS_CODEC_M_), true)
   LOCAL_CFLAGS += -DDTS_CODEC_M_
 endif
 
+ifeq ($(BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21), true)
+# This needs flag requires the following string constant in
+# CameraParametersExtra.h:
+#
+# const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "nv21";
+LOCAL_CFLAGS += -DUSE_SAMSUNG_CAMERAFORMAT_NV21
+endif
+
 # FFMPEG plugin
 LOCAL_C_INCLUDES += $(TOP)/external/stagefright-plugins/include
 
